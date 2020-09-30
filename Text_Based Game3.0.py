3cafe0983f10b540ced77e91b4afe9ae0c204139
MarydeM <maryevelyndemarigny@gmail.com>
2020-09-30 10:44:19 -0500

    comment/format  changes

--- Text_Based Game3.0.py
+++ Text_Based Game3.0.py
@@ -3,7 +3,6 @@
 # Date:09/29/2020
 # Description: Text Based Game_Updated: Python 3.8
 ######################################################################
-######################################################################
 
 #I added to my game the following features:
     #item descriptions change when grabbables are picked up
@@ -14,53 +13,53 @@
     #there is a chest that can only be open once the key is obtained
     #there is a secret room that is revealed when the book is placed on the bookshelf
     
-
-##### My additions are denoted with 5 pound signs at the beginning and end of the line #####
-
-from time import time ##### imported so a timer can be set #####
+#Used for the timer on the Dragon
+from time import time
 
 class Room(object):
     
-    def __init__(self, name): #the constructor 
+    #the constructor 
+    def __init__(self, name):
         self.name = name
-        self.exits = {} #Exit Dictionary
-        self.items = {} #Exit Dictionary
+        #Dictionaries
+        self.exits = {}
+        self.items = {}
         self.grabbables = []
         
+    #mutators and grabers
+    @property
+    def name(self):                   
+        return self._name             
+                                      
+    @name.setter                      
+    def name(self, value):            
+        self._name = value            
+                                      
+    @property                         
+    def exits(self):                  
+        return self._exits            
+                                      
+    @exits.setter                     
+    def exits(self, value):           
+        self._exits = value           
+                                      
+    @property                         
+    def items(self):                  
+        return self._items            
+                                      
+    @items.setter                     
+    def items(self, value):           
+        self._items = value           
+                                      
+    @property                         
+    def grabbables(self):             
+        return self._grabbables       
+                                      
+    @grabbables.setter                
+    def grabbables(self, value):      
+        self._grabbables = value      
         
-    @property                         #mutators and grabers
-    def name(self):                   # |
-        return self._name             # |
-                                      # |
-    @name.setter                      # |
-    def name(self, value):            # |
-        self._name = value            # |
-                                      # |
-    @property                         # |
-    def exits(self):                  # |
-        return self._exits            # |
-                                      # |
-    @exits.setter                     # |
-    def exits(self, value):           # |
-        self._exits = value           # |
-                                      # |
-    @property                         # |
-    def items(self):                  # |
-        return self._items            # |
-                                      # |
-    @items.setter                     # |
-    def items(self, value):           # |
-        self._items = value           # |
-                                      # |
-    @property                         # |
-    def grabbables(self):             # |
-        return self._grabbables       # |
-                                      # |
-    @grabbables.setter                # |
-    def grabbables(self, value):      # V
-        self._grabbables = value      #___
-        
-#-------------------------------------------------------------------
+    # Class functions
         
     def addExit(self, exit, room):
         self._exits[exit] = room
@@ -87,92 +86,114 @@ class Room(object):
     
     #Exit Class
 
+#Creates the rooms and floor layout
 def createRooms():
+    
+    #names are made global so they can be used elsewhere
     global currentRoom
-    global r1 #### I made the room names global so I can use them in a different function. #####
+    global r1
     global r2
     global r3
     global r4
     global r5
     global r6
 
-    r1 = Room("the livingroom") ##### I changed the name of the rooms just for fun #####
+    #room names
+    r1 = Room("the livingroom")
     r2 = Room("the bedroom")
     r3 = Room("the office")
     r4 = Room("the spare room")
-    r5 = Room("the Dungeon") ##### I added a basement #####
-    r6 = Room("the secret room") ##### A secret room behind the bookshelf #####
-#---------------------------------
-    r1.addExit("east", r2) # -> to the east of room 1 is room 2
-    r1.addExit("south", r3) #<- Actual Parameters assigned to the Formal Parameters
+    r5 = Room("the Dungeon")
+    r6 = Room("the secret room")
+
+    #room 1
+    r1.addExit("east", r2)
+    r1.addExit("south", r3)
     r1.addGrabbable("key")
     r1.addItem("chair", "It is made of wicker and no one is sitting on it.")
     r1.addItem("table", "It is made of oak. A golden key rests on it.")
-#---------------------------------
+
+    #room 2
     r2.addExit("west", r1)
     r2.addExit("south", r4)
     r2.addGrabbable("gun")
-    r2.addItem("closet", "There's just a dirty, old coat with a gun in the pocket") 
+    r2.addItem("closet", "There's just a dirty, old coat with a gun in \
+               the pocket") 
     r2.addItem("rug", "It is nice and Indian. It also needs to be vacuumed.")
     r2.addItem("fireplace", "It is full of ashes.")
-#---------------------------------
+
+    #room 3
     r3.addExit("north", r1)
     r3.addExit("east", r4)
     r3.addGrabbable("book")
     r3.addItem("bookshelves", "They are empty. Go figure.")
     r3.addItem("statue", "There is nothing special about it.")
     r3.addItem("desk", "The statue is resting on it. So is a book.")
-#---------------------------------
+
+    # room 4
     r4.addExit("north", r2)
     r4.addExit("west", r3)
     r4.addExit("south", r5) 
     r4.addGrabbable("6-pack")
-    r4.addItem("brew_rig", "Gourd is brewing some sort of oatmeal stout on the brew rig. A 6-pack is resting beside it.")
-#---------------------------------
+    r4.addItem("brew_rig", "Gourd is brewing some sort of oatmeal stout on \
+               the brew rig. A 6-pack is resting beside it.")
+
+    #room 5
     r5.addExit("north", r4)
-    r5.addItem("dragon!!!", "Does it matter what it looks like?! Kill it!") ##### I added a dragon to the basement #####
-#---------------------------------
+    r5.addItem("dragon!!!", "Does it matter what it looks like?! Kill it!")
+    
+    #room 6
     r6.addExit("east", r3)
-    r6.addItem("safe", "It has a dial on it. I wonder what the code could be.") ##### I added a dragon to the basement #####
-#---------------------------------
+    r6.addItem("safe", "It has a dial on it. I wonder what the code could be.")
+
     currentRoom = r1
 
 
-# ____Room Editor___________________
-def roomEdit(): ##### This edits the description of items once a grabable is picked up #####
+# edits the description of items once a grabable is in player's inventory
+def roomEdit():
     if currentRoom == r1:
         if "key" in inventory:
-            i = r1.itemDescriptions.index("It is made of oak. A golden key rests on it.")
+            i = r1.itemDescriptions.index("It is made of oak. A golden key \
+                                          rests on it.")
             r1.itemDescriptions [i] = ("It is made of oak. Nothing is on it")
             
     if currentRoom == r2:
         if "gun" in inventory:
-            i = r2.itemDescriptions.index("There's just a dirty, old coat with a gun in the pocket")
+            i = r2.itemDescriptions.index("There's just a dirty, old coat \
+                                          with a gun in the pocket")
             r2.itemDescriptions [i] = ("There's just a dirty, old coat")
     
     if currentRoom == r3:
         if "book" in inventory:
-            i = r3.itemDescriptions.index("The statue is resting on it. So is a book.")
+            i = r3.itemDescriptions.index("The statue is resting on it. So is \
+                                          a book.")
             r3.itemDescriptions [i] = ("The statue is resting on it")
             
     if currentRoom == r4:
         if "6-pack" in inventory:
-            i = r4.itemDescriptions.index( "Gourd is brewing some sort of oatmeal stout on the brew rig. A 6-pack is resting beside it.")
-            r4.itemDescriptions [i] = ("Gourd is brewing some sort of oatmeal stout on the brew rig. Did you really take his beer?")
+            i = r4.itemDescriptions.index( "Gourd is brewing some sort of \
+                                          oatmeal stout on the brew rig. A \
+                                              6-pack is resting beside it.")
+            r4.itemDescriptions [i] = ("Gourd is brewing some sort of oatmeal\
+                                        stout on the brew rig. Did you really \
+                                            take his beer?")
 
     if currentRoom == r5:
         if dragonDead == True:
-            i = r5.itemDescriptions.index("Does it matter what it looks like?! Kill it!")
+            i = r5.itemDescriptions.index("Does it matter what it looks \
+                                          like?! Kill it!")
             r5.itemDescriptions [i] = ("Yup, that's a dead dragon, alright")
-            r5.items [i] = "dead_dragon" ##### changes the name of the dragon once killed #####
-            r5.addItem("chest", "It is very large and appears to be locked") ##### reveals a previously hidden item #####
-            
-        
-#_____Shoot Enabler____________________
-##### This makes it so we can shoot the dragon and get a response #####       
-shots = 0  ##### Setting starting game variables #####
+            #changes the name of the dragon once killed
+            r5.items [i] = "dead_dragon"
+            #reveals a previously hidden item
+            r5.addItem("chest", "It is very large and appears to be locked")
+           
+#setting starting game variables 
+shots = 0
 dragonDead = False
 characterDead = False
+
+#enables dragon shooting abilities 
 def shoot():
     global start
     global shots 
@@ -199,9 +220,7 @@ def shoot():
     elif shots > 6:
         return "He's already dead, you psychopath"              
 
-    
-# ____Death_Function___________________
-    
+#displays a jolly roger when the player dies
 def death():
     print (" " * 17 + "u" * 7)
     print (" " * 13 + "u" * 2 + "$" * 11 + "u" * 2)
@@ -230,8 +249,7 @@ def death():
     print (" " * 3 + "\"" + "$" * 5 + "\"" + " " * 22 + "\"\"" + "$" * 4 + "\"\"")
     print (" " * 5 + "$" * 3 + "\"" + " " * 25 + "$" * 4 + "\"")
 
-# ____Dragon_Function___________________
-        
+#displays a dragon when it is encountered        
 def dragon(): ##### This Prints a Dragon #####
     global start 
     start = time() ##### Starts a timer #####
@@ -250,8 +268,7 @@ def dragon(): ##### This Prints a Dragon #####
     print (" " * 11 + "\   `'  / \  `'  / \  `.' /"          )    
     print (" " * 12 + "`.___,'   `.__,'   `.__,'")
     
-# ____Chest_Function___________________
-    
+#displays a chest when it is opened
 def chest():##### This prints a chest when the game is won #####
     print ("*******************************************************************************")
     print ("          |                   |                  |                     |")
@@ -282,37 +299,52 @@ inventory = []
 createRooms()
 game = "playing"
 
-##### Intro #####
-print ("Welcome! Have a look around, and you never know what you might find. Access your available options at any time by typing \"options\"")
+#game introduction
+print ("Welcome! Have a look around, and you never know what you might find. \
+       Access your available options at any time by typing \"options\"")
 
+#displays character's status
 while (True):
-    if shots == 0 or dragonDead == True: ##### Adding this makes it so the room information is not printed after every shot is fired at the dragon #####
+    #as long as the dragon is not being fought, so the room information is \
+    #not printed after every shot is fired at the dragon
+    if shots == 0 or dragonDead == True:
         status = "{}\nYou are carrying: {}\n".format(currentRoom, inventory)
-        if game == "over": ##### Ends the game once the gold is found #####
+        #ends the game once the gold is found
+        if game == "over":
             print ("You are carrying: {}".format(inventory))
             break #Exits the game
         print ("=========================================================")
         print (status)
     
+    #gets player input on what to do
     action = input("What to do? ")
+    #changes input to all lowercase for legibility
     action = action.lower()
-     
-    if (characterDead == True): ##### Executed the death function when killed by dragon #####
+    
+    #executes the death function when killed by dragon
+    if (characterDead == True):
         death()
         print ("You died")
         break
     
+    #default response
     response = ("I don't understand. Try verb noun.")
     
-    if (action == "quit" or action == "exit" or action == "bye" or action == "end"): 
-        response = "Quitting already? That's no fun. Goodbye" #### I added a string to display upon quitting. ####
+    #allows player to exit the game
+    if (action == "quit" or action == "exit" or action == "bye" or \
+        action == "end"): 
+        response = "Quitting already? That's no fun. Goodbye"
         print (response)
         break
     
-    if (action == "options"): ##### Allows the player to look at options at any time #####
-        response = ("Your verb options are go, look, take, open, place, and shoot. \n Your noun options are {} {}".format(inventory, currentRoom.items))
+    #displays the options at the player's request
+    if (action == "options"):
+        response = ("Your verb options are go, look, take, open, place, and \
+                    shoot. \n Your noun options are {} {}".format(inventory, \
+                        currentRoom.items))
     
-    
+    #controlls all actions/ executes player input
+    #splits input into a list
     words = action.split()
     if (len(words) == 2):
         verb = words[0]
@@ -341,7 +373,8 @@ while (True):
                     inventory.append(grabbable)
                     currentRoom.delGrabbable(grabbable)
                     response = "Item grabbed."
-                    roomEdit() ##### Runs the function that changes the item descsriptions #####
+                    #runs the function that changes the item descsriptions
+                    roomEdit()
                     break
                 
         elif (verb == "open" or verb == "unlock"): ##### Opens the chest behind the dragon #####
